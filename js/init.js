7d04db016c43816c8ab6ba451d13c8eeba319734
penelopelamb <smileyfacelamb@gmail.com>
2022-05-24 18:57:01 -0700

    7pm

--- js/init.js
+++ js/init.js
@@ -1,5 +1,5 @@
 
-let mapOptions = {'center': [34.0709,-118.444],'zoom':5}
+let mapOptions = {'center': [34.0709,-118.444],'zoom':15.3}
 
 
 const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
@@ -8,6 +8,11 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTnggxP8FVpcEJZvJdgs_G1BLvmAX2P_UN5CypTH549bRI5Ml3ovakKx7EIAHKc1fRlLt-eNr49wPiK/pub?output=csv"
+const q1 = 'What deters you from recycling and/or composting at UCLA?';
+const q2 = 'Which location on or near campus would you MOST like to see more recycling/compost bins on campus?';
+const q3 = 'Why do you think that UCLA has not met its Zero Waste Goal in the past?';
+const q4 = 'What else can UCLA/LA do better to encourage waste reduction and make recycling/composting more accessible?';
+
 //LOAD RESPONSES
 
 function loadData(url){
@@ -18,52 +23,58 @@ function loadData(url){
     })
 }
 
-
-
 function processData(results){
     console.log(results)
     results.data.forEach(data => {
+        //how to check if there was a response?
         console.log(data)
-        div = document.createElement("div");
-        div.classList.add("mySlides");
-        div.innerHTML = data['Where do you live?'];
-        container.appendChild(div)
+        addMarker(data)
+        addSlide(data)
     })
 }
 
 //MAP
 
 //BUTTONS
-container = document.querySelector(".slideshow-container")
+
 
 function addSurvey(title){
     const newButton = document.createElement("button"); 
     newButton.id = "button"+title; 
     newButton.innerHTML = title; 
-    newButton.style.background = 'pink';
+    newButton.style.fontSize = '50px'; 
     newButton.addEventListener('click', function(){
         window.open('https://forms.gle/SkLiuAeYRMSoi3F5A', '_blank'); 
     })
-    document.getElementById("space_for_buttons").appendChild(newButton);
+    document.getElementById("survey").appendChild(newButton);
 }
 
 // create a function to add markers
-function addMarker(lat,lng,title,message){
-    console.log(message)
-    L.marker([lat,lng]).addTo(map).bindPopup(`<h2>${title}</h2> <h3>${message}</h3>`)
-    return message
+function addMarker(data){
+    L.marker([data.lat,data.lng]).addTo(map)
 }
 
 loadData(dataUrl)
-addSurvey('Take our survey!')
+addSurvey('Take the survey!')
 
 
 //SLIDESHOW
+container = document.querySelector(".slideshow-container")
+function addSlide(data){
+    div = document.createElement("div");
+    div.classList.add("mySlides");
+    text = document.createElement("text");
+    text.classList.add("myText");
+    text.innerHTML = data[q1];
+    div.appendChild(text);
+    container.appendChild(div);
+}
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides(slideIndex += n)
 }
 
 function currentSlide(n) {
